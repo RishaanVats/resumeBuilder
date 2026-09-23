3a5501a7c99dbfc50018fcdf11c9e8eb93c418f8
RishaanVats <mishra00730@gmail.com>
2026-09-24 01:23:22 +0530

    configured app routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Home } from './shared/components/home/home';
+import { PersonalInfo } from './shared/components/personal-info/personal-info';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'personal-info', component: PersonalInfo },
+  { path: '**', redirectTo: '' },
+];
