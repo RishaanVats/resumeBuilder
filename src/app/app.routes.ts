b5845cdd8fd1abd0da8d24bc1141bee1b511589d
RishaanVats <mishra00730@gmail.com>
2026-09-26 13:59:05 +0530

    added work experience component - yet to be drafted

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { Home } from './shared/components/home/home';
 import { PersonalInfo } from './shared/components/personal-info/personal-info';
 import { ProfessionalSummary } from './shared/professional-summary/professional-summary';
+import { WorkExperience } from './shared/components/work-experience/work-experience';
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'personal-info', component: PersonalInfo },
   { path: 'professional-summary', component: ProfessionalSummary },
+  { path: 'work-experience', component: WorkExperience },
   { path: '**', redirectTo: '' },
 ];
